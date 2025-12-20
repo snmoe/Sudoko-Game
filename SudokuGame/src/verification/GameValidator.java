9fa85c9c0d3aaa18317eb80af5c501b40792cec4
Moatez-000 <moateez.ahmed@gmail.com>
2025-12-21 01:38:23 +0200

    Update GameValidator.java

--- SudokuGame/src/verification/GameValidator.java
+++ SudokuGame/src/verification/GameValidator.java
@@ -1,12 +1,28 @@
-
+    
 package verification;
 
 import java.util.ArrayList;
+import model.Game;
 
 
 public class GameValidator {
-    public static VerificationEnum validateFromResults(ArrayList<FailedVerificationResult> results){
+    public static VerificationEnum validateFromResults(Game game){
         
-        return null;
+         int[][] grid = game.getGrid();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (grid[i][j] == 0) {
+                    return VerificationEnum.INCOMPLETE;
+                }
+            }
+        }
+        SequentialVerificationMode verifier = new SequentialVerificationMode();
+        ArrayList<FailedVerificationResult> failures = verifier.verify(game);
+        if(!failures.isEmpty()){
+            return VerificationEnum.INVALID;
+        }
+        
+        
+        return VerificationEnum.VALID;
     }
 }
