36650180cd96bc22ff33bd7875d561917a57d7e7
mohand <mohandamr2019@gmail.com>
2025-12-21 22:56:28 +0200

    changed the failed verification result behaviour

--- SudokuGame/src/controllers/VerifyController.java
+++ SudokuGame/src/controllers/VerifyController.java
@@ -1,9 +1,10 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 import model.Game;
-import verification.FailedVerificationResult;
 import verification.DuplicationVerificationMode;
+import verification.FailedVerificationResult;
 import verification.GameValidator;
 import verification.VerificationEnum;
 
@@ -12,19 +13,31 @@ public class VerifyController {
     public String verify(Game game) {
 
         VerificationEnum status = GameValidator.validate(game);
-      
 
         if (status == VerificationEnum.VALID) {
             return "VALID";
         }
         if (status == VerificationEnum.INCOMPLETE) {
             return "INCOMPLETE";
-        }
-        else{
+        } else {
             return "INVALID";
         }
+   }
+
+    public List<int[]> getInvalidAbsolutePositions(Game game) {
+        DuplicationVerificationMode mode = new DuplicationVerificationMode();
+        List<FailedVerificationResult> results = mode.verify(game);
+        ArrayList<int[]> positions = new ArrayList<>();
         
-      
+        for(FailedVerificationResult result : results){
+            ArrayList<int[]> resultPositions = (ArrayList<int[]>) result.getAbsolutePositions();
+            for(int[] position : resultPositions){
+                positions.add(position);
+            }
+        }
+        
+        
+        return positions;
     }
 
 }
